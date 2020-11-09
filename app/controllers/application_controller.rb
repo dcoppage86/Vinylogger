cb7988b904af189a764c37bc251a453c7f4939a0
dcoppage86 <dakotakcoppage@me.com>
2020-11-09 01:24:10 -0700

    created models, views, controllers and migrated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, 'password_security'
   end
 
   get "/" do
